cc288ad356d84f22396c8c92614398035d7e5013
John Fawcett <jrf0110@gmail.com>
2012-11-14 12:32:41 -0600

    consolidated

---
+++ src/radagast.js
@@ -0,0 +1,8 @@
+define(function(require){
+  return {
+    utils  : require('./utils')
+  , apps   : require('./apps-handler')
+  , App    : require('./app')
+  , Suite  : require('./suite')
+  };
+});
