a66cadccb1128d08346225122fb41d391f025737
John Fawcett <jrf0110@gmail.com>
2012-11-28 17:00:36 -0600

    fixed landing-app

--- examples/goodybag-site/apps/landing-site/views/app.js
+++ examples/goodybag-site/apps/landing-site/views/app.js
@@ -1,19 +1,60 @@
 define(function(require){
   var
-    rad   = require('radagast')
-  // , utils = require('utils')
+    rad         = require('radagast')
+  , utils       = require('utils')
+
+  , NavView     = require('./nav')
+  , FooterView  = require('./footer')
+  , LandingView = require('./landing-page')
+
+  , template    = require('hbt!./../html/app')
+
+  , logger      = new rad.logger("Landing Site")
   ;
 
   return rad.App.extend({
     regions: {
-      '.test-app': 'test-app'
+      '.main-nav' :  new NavView()
+    , '.footer'   :  new FooterView()
+    }
+
+  , pages: {
+      'landing': new LandingView()
     }
 
   , initialize: function(){
-
+      var this_ = this, bound = function(fn){
+        return function(){ fn.apply(this_, arguments); }
+      };
+      rad.subscribe('landing.changePageRequest', bound(this.onChangePageRequest));
     }
+
   , render: function(){
-      this.$el.html('<h1>Landing Site</h1><div class="test-app"></div>');
+      this.$el.html(template());
+
+      var $pages = this.$el.find('.pages'), page;
+
+      for (var key in this.pages){
+        page = this.pages[key];
+        page.render();
+        page.$el.css('display', 'none');
+        $pages.append(page.$el);
+      }
+    }
+
+  , onChangePageRequest: function(channel, page){
+      return this.changePage(page);
+    }
+
+  , changePage: function(pageName){
+      if (!this.pages[pageName]) return logger.error("Page does not exist: ", pageName), this;
+
+      if (this.currentPage === pageName) return logger.warn("Already on this page: ", pageName), this;
+
+      logger.info("Changing to page: ", pageName);
+
+      if (this.currentPage) this.pages[this.currentPage].$el.css('display', 'none');
+      this.pages[this.currentPage = pageName].$el.css('display', 'block');
     }
   });
 });
